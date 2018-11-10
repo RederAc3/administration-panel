98857437307125ec5570005ecd72fc9e1af4a635
Andrzej Kot <reder.ac3@gmail.com>
2018-11-10 23:45:18 +0100

    Add subpage personal data

--- js/script.js
+++ js/script.js
@@ -1,4 +1,124 @@
+//function closeModal() {
+//  document.getElementById('overlay').classList.add('opacity-off')
+//};
+//
+//document.querySelector('.overlay').addEventListener('click', function(e) {
+//  if(e.target === this) {
+//    closeModal();
+//  }
+//});
+//
+//document.addEventListener('keyup', function(e) {
+//  if(e.keyCode === 27) {
+//    closeModal();
+//  }
+//});
+//
+//function openModal(modal) {
+//  document.querySelectorAll('.overlay > *').forEach(function(modal) {
+//    modal.classList.remove('opacity-on')
+//  })
+//  document.querySelector('.overlay').classList.add('opacity-on')
+//  document.querySelector(modal).classList.remove('opacity-on')
+//};
+//
+//openModal('.login-modal')
+//
+//document.getElementByClassName('logout') .addEventListener('click', function (event) {
+//    
+//    openModal();
+//});
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//'use strict'
+//
+//var quit = document.getElementsByClassName('logout');
+//var modal = document.getElementsByClassName('modal-container-quit');
+//var buttonQuit = document.getElementsByClassName('button-quit');
+//var buttonCancel = document.getElementsByClassName('button-cancel');
+//var opacityOn = document.getElementsByClassName('opacity-on');
+//var opacityOff = document.getElementsByClassName('opacity-off');
+//
+//var hidden = function () {
+//    
+//    modal.removeClass = opacityOff;
+//    modal.className = opacityOn;
+//    
+//};
+//
+//var show = function () {
+//    
+//    modal.removeClass = opacityOn;
+//    modal.className = opacityOff;
+//};
+//
+//show();
+//
+//quit.addEventListener('click', function (event) {
+//    
+//    show();
+//    
+////    console.log("he");
+//});
+//
+//buttonCancel.addEventListener('click', function (event) {
+//    hidden();
+////    console.log(quit);
+//    
+//});
+//
+
+
+
+
+
+
+
+
 //var add = document.getElementsByClassName('line');
 //var button = document.getElementsByClassName('button-add');
-//var name = 
+//var name = ;
+//    
+//var addLink = function(){
+//    button.addEventListener('click' function(event) {
+//        if ()
+//    })
+//}
+
 
