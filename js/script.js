272a7e048a6e82c4b75768db23920e7e1198c295
Andrzej Kot <reder.ac3@gmail.com>
2018-11-15 22:44:47 +0100

    Add all subpages and script modal

--- js/script.js
+++ js/script.js
@@ -1,124 +1,126 @@
-//function closeModal() {
-//  document.getElementById('overlay').classList.add('opacity-off')
-//};
-//
-//document.querySelector('.overlay').addEventListener('click', function(e) {
-//  if(e.target === this) {
-//    closeModal();
-//  }
-//});
-//
-//document.addEventListener('keyup', function(e) {
-//  if(e.keyCode === 27) {
-//    closeModal();
-//  }
-//});
-//
-//function openModal(modal) {
-//  document.querySelectorAll('.overlay > *').forEach(function(modal) {
-//    modal.classList.remove('opacity-on')
-//  })
-//  document.querySelector('.overlay').classList.add('opacity-on')
-//  document.querySelector(modal).classList.remove('opacity-on')
-//};
-//
-//openModal('.login-modal')
-//
-//document.getElementByClassName('logout') .addEventListener('click', function (event) {
-//    
-//    openModal();
-//});
-//
-//
+'use strict'
+$(function () {
+    $('.select').datepicker();
+});
+
+var loginModal = document.getElementById('container-modal-login');
+var quitModal = document.getElementById('container-modal-quit');
+
+function showModalLogin() {
+    loginModal.classList.remove('opacity-on')
+    loginModal.classList.add('opacity-off')
+
+};
+
+function closeModalLogin() {
+    loginModal.classList.remove('opacity-off')
+    loginModal.classList.add('opacity-on')
+
+};
+
+function showModalQuit() {
+    quitModal.classList.remove('opacity-on')
+    quitModal.classList.add('opacity-off')
+
+};
+
+function closeModalQuit() {
+    quitModal.classList.remove('opacity-off')
+    quitModal.classList.add('opacity-on')
+
+};
+
+document.querySelector('.account').addEventListener('click', function (event) {
+    showModalLogin();
+    console.log(1);
+});
+
+document.querySelector('.overlay-login').addEventListener('click', function (event) {
+
+    closeModalLogin();
+    console.log(2);
+});
 
 
 
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModalLogin();
+        console.log(4);
+    }
+});
+
+
+document.querySelector('.logout').addEventListener('click', function (event) {
+    showModalQuit();
+    console.log(5);
+});
+
+document.querySelector('.overlay-quit').addEventListener('click', function (event) {
+
+    closeModalQuit();
+    console.log(2);
+});
+
+document.querySelector('.button-cancel').addEventListener('click', function (event) {
+
+    closeModalQuit();
+    console.log(2);
+});
+
+document.querySelector('.button-quit').addEventListener('click', function (event) {
+
+    closeModalQuit();
+    showModalLogin();
+    console.log(2);
+});
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode === 27) {
+        closeModalQuit();
+        console.log(6);
+    }
+});
 
 
 
+//###########################--WYKRES--##################################
 
 
+var ctx = document.getElementById('myChart').getContext('2d');
 
+var chart = new Chart(ctx, {
+    
 
+    type: 'bar',
+    data: {
 
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
 
+        datasets: [{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//'use strict'
-//
-//var quit = document.getElementsByClassName('logout');
-//var modal = document.getElementsByClassName('modal-container-quit');
-//var buttonQuit = document.getElementsByClassName('button-quit');
-//var buttonCancel = document.getElementsByClassName('button-cancel');
-//var opacityOn = document.getElementsByClassName('opacity-on');
-//var opacityOff = document.getElementsByClassName('opacity-off');
-//
-//var hidden = function () {
-//    
-//    modal.removeClass = opacityOff;
-//    modal.className = opacityOn;
-//    
-//};
-//
-//var show = function () {
-//    
-//    modal.removeClass = opacityOn;
-//    modal.className = opacityOff;
-//};
-//
-//show();
-//
-//quit.addEventListener('click', function (event) {
-//    
-//    show();
-//    
-////    console.log("he");
-//});
-//
-//buttonCancel.addEventListener('click', function (event) {
-//    hidden();
-////    console.log(quit);
-//    
-//});
-//
-
-
-
-
-
-
-
-
-//var add = document.getElementsByClassName('line');
-//var button = document.getElementsByClassName('button-add');
-//var name = ;
-//    
-//var addLink = function(){
-//    button.addEventListener('click' function(event) {
-//        if ()
-//    })
-//}
-
-
+                label: "Signups",
+                backgroundColor: '#8DBEC8',
+                borderColor: '#8DBEC8',
+                data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+        },
+            {
+                label: "FTD",
+                backgroundColor: '#F29E4E',
+                borderColor: '#F29E4E',
+                data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
+        },
+            {
+                label: "Earned",
+                backgroundColor: '#71B374',
+                borderColor: '#71B374',
+                data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
+                hidden: true,
+        }]
+    },
+    
+    options: {
+      events: ['click']  
+    },
+    
+});
